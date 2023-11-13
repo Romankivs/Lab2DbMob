fb9deaf3ced8323dc9a4506201ca708e26a15552
Sviatoslav Romankiv <sviatoslav@source-elements.com>
2023-11-13 23:13:25 +0200

    Initial Commit

---
+++ Lab2DBMob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab2DBMob
+//
+//  Created by Sviatoslav Romankiv on 13.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab2DBMob/Lab2DBMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2DBMobApp.swift
+//  Lab2DBMob
+//
+//  Created by Sviatoslav Romankiv on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab2DBMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
